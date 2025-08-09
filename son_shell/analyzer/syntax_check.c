7ac615c947f7b12acc04382a662a8e643cdc8246
Samed763 <sdinc763@gmail.com>
2025-08-09 16:28:00 +0300

    syntax_fix

--- son_shell/analyzer/syntax_check.c
+++ son_shell/analyzer/syntax_check.c
@@ -6,44 +6,37 @@
 /*   By: sadinc <sadinc@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 19:53:19 by sadinc            #+#    #+#             */
-/*   Updated: 2025/08/09 16:00:41 by sadinc           ###   ########.fr       */
+/*   Updated: 2025/08/09 16:14:21 by sadinc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
 static int	check_token_syntax(char *line, int i)
 {
 	int	j;
 
-	// '||' gibi desteklenmeyen operatörleri kontrol et
 	if (line[i] == '|' && line[i + 1] && line[i + 1] == '|')
 		return (1); // Hata: '||' desteklenmiyor
-
-	// '>>>' veya '<<<' gibi geçersiz yönlendirmeleri kontrol et
-	if ((line[i] == '>' || line[i] == '<') && line[i+1] == line[i] && line[i+2] == line[i])
+	if ((line[i] == '>' || line[i] == '<') && line[i + 1] == line[i] && line[i
+		+ 2] == line[i])
 		return (1);
-
-	// Yönlendirme ve pipe'lardan sonra ne geldiğini kontrol et
 	j = i;
 	if ((line[j] == '<' || line[j] == '>') && line[j + 1] == line[j])
 		j++; // '<<' veya '>>' ise bir karakter atla
 	j++;
-
 	while (line[j] && (line[j] == ' ' || line[j] == '\t'))
 		j++;
-
-	// Bir token'dan sonra boşluk veya başka bir token geliyorsa, bu bir hatadır.
-	// Ancak, pipe'dan sonra bir yönlendirme token'ı gelebilir.
 	if (is_token(line[i]))
 	{
 		if (line[j] == '\0') // Satır sonu ise hata
 			return (1);
-		if (line[i] != '|' && is_token(line[j])) // Yönlendirme sonrası başka bir token gelirse hata
+		if (line[i] != '|' && is_token(line[j]))
+			// Yönlendirme sonrası başka bir token gelirse hata
 			return (1);
 		if (line[i] == '|' && line[j] == '|') // '||' durumu
 			return (1);
 	}
-
 	return (0);
 }
 
